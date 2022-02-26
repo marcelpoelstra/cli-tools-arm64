35103f1535963e3c74be6e42957035ef41c0e1b2
Marcel Poelstra <marcel.poelstra@gmail.com>
2022-02-26 16:33:10 +0100

    Updates for consistancy with the other ARM64 builds and tools

--- build-images-for-arm64.sh
+++ build-images-for-arm64.sh
@@ -1,26 +1,26 @@
 cd docker
-cd mp4split
+cd mp4split-arm64
 echo "building local docker image mp4split..."
-docker build . -t mp4split:latest
+docker build . -t mp4split-arm64:latest
 echo "complete"
-cd cpix_verify
+cd cpix_verify-arm64
 echo "building local docker image cpix_verify..."
-docker build . -t cpix_verify:latest
+docker build . -t cpix_verify-arm64:latest
 echo "complete"
-cd ../manifest_edit
+cd ../manifest_edit-arm64
 echo "building local docker image manifest_edit..."
-docker build . -t manifest_edit:latest
+docker build . -t manifest_edit-arm64:latest
 echo "complete"
-cd ../push_input_stream
+cd ../push_input_stream-arm64
 echo "building local docker image push_input_stream..."
-docker build . -t push_input_stream:latest
+docker build . -t push_input_stream-arm64:latest
 echo "complete"
-cd ../unified_capture
+cd ../unified_capture-arm64
 echo "building local docker image unified_capture..."
-docker build . -t unified_capture:latest
+docker build . -t unified_capture-arm64:latest
 echo "complete"
-cd ../unified_remix 
+cd ../unified_remix-arm64
 echo "building local docker image unified_capture..."
-docker build . -t unified_remix:latest
+docker build . -t unified_remix-arm64:latest
 echo "complete"
 echo "building docker images complete"
